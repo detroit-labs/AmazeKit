e302f86aec517512875ddbd6a681a2fdd28b16c5
Jeff Kelley <SlaunchaMan@gmail.com>
2012-08-13 11:36:41 -0400

    import header to squelch warnings.

--- AmazeKit/AmazeKit/AKDrawingUtilities.m
+++ AmazeKit/AmazeKit/AKDrawingUtilities.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 Detroit Labs. All rights reserved.
 //
 
+#import "AKDrawingUtilities.h"
+
 #import "UIImage+AKPixelData.h"
 
 CGFloat AngleBetweenTwoPoints(CGPoint innerPoint, CGPoint outerPoint)
