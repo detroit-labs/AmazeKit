d4f6d052ca62dfa0f98e3d0bf7afce0819defa38
Jeff Kelley <SlaunchaMan@gmail.com>
2012-09-06 17:34:43 -0400

    Use an NSCache to cache images without having to go to the disk every time.

--- AmazeKit/AmazeKit/AKFileManager.m
+++ AmazeKit/AmazeKit/AKFileManager.m
@@ -22,14 +22,21 @@ static const char *kPNGSoftwareValue = "AmazeKit";
 @interface AKFileManager() {
 	NSFileManager   	*_fileManager;
 	NSOperationQueue	*_imageIOQueue;
+	NSCache         	*_renderedImageCache;
 }
 
+- (NSString *)cacheKeyForHash:(NSString *)hash
+					   atSize:(CGSize)size
+					withScale:(CGFloat)scale;
+- (NSString *)dimensionsRepresentationWithSize:(CGSize)size
+										 scale:(CGFloat)scale;
 - (NSFileManager *)fileManager;
 - (NSOperationQueue *)imageIOQueue;
-
 - (NSString *)pathForHash:(NSString *)hash
 				   atSize:(CGSize)size
 				withScale:(CGFloat)scale;
+- (NSCache *)renderedImageCache;
+
 
 @end
 
@@ -117,25 +124,112 @@ static const char *kPNGSoftwareValue = "AmazeKit";
 {
 	__block BOOL imageExists = NO;
 	
-	NSString *path = [self pathForHash:descriptionHash
-								atSize:size
-							 withScale:scale];
+	NSString *cacheKey = [self cacheKeyForHash:descriptionHash
+										atSize:size
+									 withScale:scale];
 	
-	NSBlockOperation *imageExistsOperation = [NSBlockOperation blockOperationWithBlock:^{
-		imageExists = [[self fileManager] fileExistsAtPath:path];
-	}];
+	imageExists = ([[self renderedImageCache] objectForKey:cacheKey] != nil);
 	
-	[[self imageIOQueue] addOperation:imageExistsOperation];
-	[imageExistsOperation waitUntilFinished];
+	if (imageExists == NO) {
+		NSString *path = [self pathForHash:descriptionHash
+									atSize:size
+								 withScale:scale];
+		
+		NSBlockOperation *imageExistsOperation = [NSBlockOperation blockOperationWithBlock:^{
+			imageExists = [[self fileManager] fileExistsAtPath:path];
+		}];
+		
+		[[self imageIOQueue] addOperation:imageExistsOperation];
+		[imageExistsOperation waitUntilFinished];
+	}
 	
 	return imageExists;
 }
 
+- (UIImage *)cachedImageForHash:(NSString *)descriptionHash
+						 atSize:(CGSize)size
+					  withScale:(CGFloat)scale
+{
+	
+	__block UIImage *cachedImage = nil;
+	
+	if ([self cachedImageExistsForHash:descriptionHash
+								atSize:size
+							 withScale:scale] == YES) {
+		NSString *cacheKey = [self cacheKeyForHash:descriptionHash
+											atSize:size
+										 withScale:scale];
+		
+		cachedImage = [[self renderedImageCache] objectForKey:cacheKey];
+		
+		if (cachedImage == nil) {
+			NSBlockOperation *imageLoadOperation = [NSBlockOperation blockOperationWithBlock:^{
+				NSString *path = [self pathForHash:descriptionHash
+											atSize:size
+										 withScale:scale];
+				
+				NSURL *url = [NSURL fileURLWithPath:path];
+				
+				CFStringRef       myKeys[2];
+				CFTypeRef         myValues[2];
+				
+				myKeys[0] = kCGImageSourceShouldCache;
+				myValues[0] = (CFTypeRef)kCFBooleanTrue;
+				myKeys[1] = kCGImageSourceShouldAllowFloat;
+				myValues[1] = (CFTypeRef)kCFBooleanTrue;
+				
+				CFDictionaryRef options = CFDictionaryCreate(kCFAllocatorDefault,
+															 (const void **) myKeys,
+															 (const void **) myValues,
+															 2,
+															 &kCFTypeDictionaryKeyCallBacks,
+															 &kCFTypeDictionaryValueCallBacks);
+				
+				CGImageSourceRef imageSource = CGImageSourceCreateWithURL((__bridge CFURLRef)url,
+																		  options);
+				CFRelease(options);
+				
+				if (imageSource != NULL) {
+					// Create an image from the first item in the image source.
+					CGImageRef baseImage = CGImageSourceCreateImageAtIndex(imageSource,
+																		   0,
+																		   NULL);
+					
+					if (baseImage != NULL) {
+						cachedImage = [[UIImage alloc] initWithCGImage:baseImage
+																 scale:scale
+														   orientation:UIImageOrientationUp];
+						
+						CGImageRelease(baseImage);
+					}
+					
+					CFRelease(imageSource);
+				}
+			}];
+			
+			[[self imageIOQueue] addOperation:imageLoadOperation];
+			[imageLoadOperation waitUntilFinished];
+			
+			if (cachedImage != nil) {
+				[[self renderedImageCache] setObject:cachedImage
+											  forKey:[self cacheKeyForHash:descriptionHash
+																	atSize:size
+																 withScale:scale]];
+			}
+		}
+	}
+	
+	return cachedImage;
+}
+
 - (void)cacheImage:(UIImage *)image forHash:(NSString *)descriptionHash
 {
+	CGSize imageSize = [image size];
+	CGFloat imageScale = [image scale];
+	
 	NSString *path = [self pathForHash:descriptionHash
-								atSize:[image size]
-							 withScale:[image scale]];
+								atSize:imageSize
+							 withScale:imageScale];
 	
 	NSURL *url = [NSURL fileURLWithPath:path];
 	
@@ -192,8 +286,8 @@ static const char *kPNGSoftwareValue = "AmazeKit";
 		
 		if (success == false) {
 			NSLog(@"Could not cache image with size %@ and scale %.0f to path: %@",
-				  NSStringFromCGSize([image size]),
-				  [image scale],
+				  NSStringFromCGSize(imageSize),
+				  imageScale,
 				  path);
 		}
 	}];
@@ -203,6 +297,27 @@ static const char *kPNGSoftwareValue = "AmazeKit";
 	[imageWritingOperation setQueuePriority:NSOperationQueuePriorityHigh];
 	
 	[[self imageIOQueue] addOperation:imageWritingOperation];
+	
+	[[self renderedImageCache] setObject:image
+								  forKey:[self cacheKeyForHash:descriptionHash
+														atSize:imageSize
+													 withScale:imageScale]];
+}
+
+- (NSString *)cacheKeyForHash:(NSString *)hash
+					   atSize:(CGSize)size
+					withScale:(CGFloat)scale
+{
+	return [NSString stringWithFormat:@"%@%@", hash, [self dimensionsRepresentationWithSize:size scale:scale]];
+}
+
+- (NSString *)dimensionsRepresentationWithSize:(CGSize)size scale:(CGFloat)scale
+{
+    NSString *dimensionsRepresentation = [NSString stringWithFormat:@"{%dx%dx%d}",
+										  (int)size.width,
+										  (int)size.height,
+										  (int)scale];
+    return dimensionsRepresentation;
 }
 
 - (NSFileManager *)fileManager
@@ -275,10 +390,7 @@ static const char *kPNGSoftwareValue = "AmazeKit";
 		[createDirectoryOperation waitUntilFinished];
 	}
 	
-	NSString *dimensionsRepresentation = [NSString stringWithFormat:@"{%dx%dx%d}",
-										  (int)size.width,
-										  (int)size.height,
-										  (int)scale];
+	NSString *dimensionsRepresentation = [self dimensionsRepresentationWithSize:size scale:scale];
 	
 	NSString *imagePath = [[baseHashPath stringByAppendingPathComponent:dimensionsRepresentation]
 						   stringByAppendingPathExtension:@"png"];
@@ -286,64 +398,17 @@ static const char *kPNGSoftwareValue = "AmazeKit";
 	return imagePath;
 }
 
-- (UIImage *)cachedImageForHash:(NSString *)descriptionHash
-						 atSize:(CGSize)size
-					  withScale:(CGFloat)scale
+- (NSCache *)renderedImageCache
 {
-	__block UIImage *cachedImage = nil;
-	
-	if ([self cachedImageExistsForHash:descriptionHash
-								atSize:size
-							 withScale:scale] == YES) {
-		
-		NSBlockOperation *imageLoadOperation = [NSBlockOperation blockOperationWithBlock:^{
-			NSString *path = [self pathForHash:descriptionHash
-										atSize:size
-									 withScale:scale];
-			
-			NSURL *url = [NSURL fileURLWithPath:path];
-			
-			CFStringRef       myKeys[2];
-			CFTypeRef         myValues[2];
-			
-			myKeys[0] = kCGImageSourceShouldCache;
-			myValues[0] = (CFTypeRef)kCFBooleanTrue;
-			myKeys[1] = kCGImageSourceShouldAllowFloat;
-			myValues[1] = (CFTypeRef)kCFBooleanTrue;
-			
-			CFDictionaryRef options = CFDictionaryCreate(kCFAllocatorDefault,
-														 (const void **) myKeys,
-														 (const void **) myValues,
-														 2,
-														 &kCFTypeDictionaryKeyCallBacks,
-														 &kCFTypeDictionaryValueCallBacks);
-			
-			CGImageSourceRef imageSource = CGImageSourceCreateWithURL((__bridge CFURLRef)url, options);
-			CFRelease(options);
-			
-			if (imageSource != NULL) {
-				// Create an image from the first item in the image source.
-				CGImageRef baseImage = CGImageSourceCreateImageAtIndex(imageSource,
-																	   0,
-																	   NULL);
-				
-				if (baseImage != NULL) {
-					cachedImage = [[UIImage alloc] initWithCGImage:baseImage
-															 scale:scale
-													   orientation:UIImageOrientationUp];
-					
-					CGImageRelease(baseImage);
-				}
-				
-				CFRelease(imageSource);
-			}
-		}];
-		
-		[[self imageIOQueue] addOperation:imageLoadOperation];
-		[imageLoadOperation waitUntilFinished];
+	if (_renderedImageCache == nil) {
+		static dispatch_once_t onceToken;
+		dispatch_once(&onceToken, ^{
+			_renderedImageCache = [[NSCache alloc] init];
+			[_renderedImageCache setName:@"AmazeKit Rendered Image Cache"];
+		});
 	}
 	
-	return cachedImage;
+	return _renderedImageCache;
 }
 
 #pragma mark -
