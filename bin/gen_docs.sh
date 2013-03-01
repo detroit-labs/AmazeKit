854fd9481fa22db08fe0f220f3d43dc841ff3964
Jeff Kelley <SlaunchaMan@gmail.com>
2013-03-01 11:53:00 -0500

    Create doc set and HTML page.

--- bin/gen_docs.sh
+++ bin/gen_docs.sh
@@ -2,8 +2,22 @@
 
 appledoc -o docs/ \
          -h \
-	 --no-create-docset \
+	 --create-docset \
+	 --no-install-docset \
 	 --project-name AmazeKit \
 	 --project-company Detroit\ Labs \
+	 --project-version $(cat VERSION) \
+	 --company-id com.detroitlabs \
+	 --no-repeat-first-par \
+	 AmazeKit/AmazeKit/
+
+appledoc -o docs/ \
+         -h \
+	 --no-create-docset \
+	 --create-html \
+	 --project-name AmazeKit \
+	 --project-company Detroit\ Labs \
+	 --project-version $(cat VERSION) \
+	 --company-id com.detroitlabs \
 	 --no-repeat-first-par \
 	 AmazeKit/AmazeKit/
