71e3d770cff7ff90071de811a3261289cd6b30bd
Jeff Kelley <SlaunchaMan@gmail.com>
2013-03-01 14:38:25 -0500

    Modify doc-publishing script to point to the new AmazeKit org repo.

--- bin/publish_docs.sh
+++ bin/publish_docs.sh
@@ -11,21 +11,19 @@ fi
 
 pushd "${TMP_DIR}"
 
-git clone git@github.com:detroit-labs/AmazeKit
-cd AmazeKit/
-git fetch --all
-git checkout gh-pages
+git clone git@github.com:AmazeKit/amazekit.github.com
+cd amazekit.github.com/
 
 popd
 
-rsync -HhavP --delete --exclude=".git/" docs/html/ "${TMP_DIR}/AmazeKit/"
+rsync -HhavP --delete --exclude=".git/" docs/html/ "${TMP_DIR}/amazekit.github.com/"
 
-pushd "${TMP_DIR}/AmazeKit/"
+pushd "${TMP_DIR}/amazekit.github.com/"
 
 git add --all
 git commit -m "Updated docs for version ${VERSION}."
 
-git push origin gh-pages
+git push origin master
 
 popd
 
