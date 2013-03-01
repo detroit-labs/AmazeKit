9364f9cf60fe84c7df0c6173c60a518c0ffd57a2
Jeff Kelley <SlaunchaMan@gmail.com>
2013-03-01 11:42:27 -0500

    Add documentation for inner glow image effect.

--- AmazeKit/AmazeKit/AKInnerGlowImageEffect.h
+++ AmazeKit/AmazeKit/AKInnerGlowImageEffect.h
@@ -22,8 +22,33 @@
 #import "AKInnerShadowImageEffect.h"
 
 
+/** The AKInnerGlowImageEffect class creates a shadow on the inside of the image. It’s like an
+ *  inner shadow with no offset.
+ *
+ *  This class should probably be re-implemented to be a true inner glow, not an inner shadow. Pull
+ *  requests welcome.
+ */
+
+
 @interface AKInnerGlowImageEffect : AKInnerShadowImageEffect
 
+/**-----------------------------------------
+ * @name Creating an Inner Glow Image Effect
+ * -----------------------------------------
+ */
+
+/** The designated initializer for an inner glow image effect.
+ *
+ *  Because image effects are immutable (for performance reasons), you’ll need to set the proper
+ *  values for the properties at time of creation. Subclasses should specify additional parameters
+ *  in their own initializers.
+ *
+ *  @param alpha The value for the alpha property.
+ *  @param blendMode The value for the blendMode property.
+ *  @param color The color to draw the shadow.
+ *  @param radius The radius for the shadow.
+ *  @return An initialized image effect.
+ */
 - (id)initWithAlpha:(CGFloat)alpha
 		  blendMode:(CGBlendMode)blendMode
 			  color:(UIColor *)color
